4698e3e914257b91f13de6f35d3702f3150be68e
KeerthanaSriM <keerthana.pmgj@gmail.com>
2024-11-20 16:46:59 -0700

    Update cart.java

--- cart.java
+++ cart.java
@@ -7,8 +7,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextInputDialog;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.application.Application;
